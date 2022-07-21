301c6d7c158f312b5400b36b283974c6f5885e0a
AlexOscar <alejandro.garcia.carmona@gmail.com>
2022-07-21 05:24:30 +0200

    Card

--- src/App.js
+++ src/App.js
@@ -1,14 +1,27 @@
-import logo from "./logo.svg";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap";
 import React, { useState, useEffect } from "react";
 import Card from "./components/Card/Card";
-import Filter from "./components/Filter/Filter";
-import Navbar from "./components/Navbar/Navbar";
-import Pagination from "./components/Pagination/Pagination";
-import Search from "./components/Search/Search";
+
+// import Filter from "./components/Filter/Filter";
+// import Navbar from "./components/Navbar/Navbar";
+// import Pagination from "./components/Pagination/Pagination";
+// import Search from "./components/Search/Search";
+
 
 function App() {
+  
+  let api = `https://rickandmortyapi.com/api/character/?page=1`;
+  let [fetchedData, updateFetchedData] = useState([]);
+  let {info, results} = fetchedData;
+
+  useEffect(() => {
+    (async function(){
+      let data = await fetch(api).then((res) => res.json());
+      updateFetchedData(data);
+    })();
+   }, [api]);
+
   return (
     <div className="App">
       <h1 className="text-center mb-3">Characters</h1>
@@ -16,7 +29,9 @@ function App() {
         <div className="row">
           Filter component will be placed here
           <div className="col-lg-8 col-12">
-            <div className="row">Card component will be placed here</div>
+            <div className="row">
+              <Card results={results} />
+            </div>
           </div>
         </div>
       </div>
