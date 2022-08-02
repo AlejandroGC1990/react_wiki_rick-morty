3d31cdc1d876b3a3e56d48801fbaf9421613e7c0
AlexOscar <alejandro.garcia.carmona@gmail.com>
2022-08-02 21:03:32 +0200

    Navbar and fixes in app

--- src/App.js
+++ src/App.js
@@ -1,19 +1,30 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap";
-import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
 import React, { useState, useEffect } from "react";
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import Navbar from "./components/Navbar/Navbar";
 import Card from "./components/Card/Card";
 import Episodes from "./Pages/Episodes";
 import Filter from "./components/Filter/Filter";
 import Location from "./Pages/Location"
-import Navbar from "./components/Navbar/Navbar";
 import Pagination from "./components/Pagination/Pagination";
 import Search from "./components/Search/Search";
 
+function App() {
+  <Router>
+      <div className="App">
+        <Navbar/>
+      </div>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/episodes" element={<Episodes/>}/>
+        <Route path= "/location" elemental={<Location/>}/>
+      </Routes>
+  </Router>
+}
+
 const Home = () => {
 
-  function App() {
-  
     let [fetchedData, updateFetchedData] = useState([]);
     let {info, results} = fetchedData;
     let [pageNumber, updatePageNumber] =useState(1);
@@ -30,18 +41,8 @@ const Home = () => {
       })();
      }, [api]);
 
-
-
     return (
-      <div className="App">
-        <Router>
-          <div className="App">
-            <Navbar/>
-            <Route path="/" element={<Home/>}/>
-            <Route path="/episodes" element={<Episodes/>}/>
-            <Route path= "/location" elemental={<Location/>}/>
-          </div>
-        </Router>
+      <div className="App">  
         <h1 className="text-center mb-3">Characters</h1>
         <Search setSearch={setSearch} updatePageNumber={updatePageNumber}/>
         
@@ -50,6 +51,7 @@ const Home = () => {
           <Filter
             pageNumber={pageNumber}
             status={status}
+            updateGender={updateGender}
             updateStatus={updateStatus}
             updateSpecies={updateSpecies}
             updatePageNumber={updatePageNumber}
@@ -68,10 +70,6 @@ const Home = () => {
         />
       </div>
     );
-  }
-
 }
 
-
-
-export default Home;
+export default App;
