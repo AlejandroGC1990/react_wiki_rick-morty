cb909f3d6dc4fbec2bd2a19376d43624fd11b67c
AlexOscar <alejandro.garcia.carmona@gmail.com>
2022-08-04 20:02:57 +0200

    Card-page

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,7 +1,8 @@
 import React from "react";
 import styles from "./Card.module.scss";
+import { Link } from "react-router-dom";
 
-const Card = ({results}) => {
+const Card = ({page, results}) => {
     let display;
 
     if (results) {
@@ -9,45 +10,51 @@ const Card = ({results}) => {
             let {id, image, name, status, location} = x;        
 
             return (
-                
-
-                <div
-                    key = {id}
+                <Link
+                    style={{ textDecoration: "none" }}
+                    to={`${page}${id}`}
+                    key={id}
                     className="col-lg-4 col-md-6 col-sm-6 col-12 mb-4 position-relative text-dark"
                 >
-                    <div className={`${styles.card} d-flex flex-column justify-content-center`}>
-                        <img className={`${styles.img} img-fluid`} src = {image} alt="" />
-                        <div className={`${styles.content}`}>
-                            <div className="fs-5 fw-bold mb-4">{name}</div>
-                            <div className="">
-                                <div className="fs-6 fw-normal">Last location</div>
-                                <div className="fs-5">{location.name}</div> 
+
+                    <div
+                        key = {id}
+                        className="col-lg-4 col-md-6 col-sm-6 col-12 mb-4 position-relative text-dark"
+                    >
+                        <div className={`${styles.card} d-flex flex-column justify-content-center`}>
+                            <img className={`${styles.img} img-fluid`} src = {image} alt="" />
+                            <div className={`${styles.content}`}>
+                                <div className="fs-5 fw-bold mb-4">{name}</div>
+                                <div className="">
+                                    <div className="fs-6 fw-normal">Last location</div>
+                                    <div className="fs-5">{location.name}</div> 
+                                </div>
                             </div>
                         </div>
-                    </div>
 
-                    {(() => {
-                    if (status === "Dead") {
-                        return (
-                            <div className={`${styles.badge} position-absolute badge bg-danger`}>
-                                {status}
-                            </div>
-                        );
-                    } else if (status === "Alive") {
-                        return (
-                            <div className={`${styles.badge} position-absolute badge bg-success`}>
-                                {status}
-                            </div>
-                        );
-                    } else {
-                        return (
-                            <div className={`${styles.badge} position-absolute badge bg-secondary`}>
-                                {status}
-                            </div>
-                        );
-                    }
-                    })()}
-                </div>     
+                        {(() => {
+                        if (status === "Dead") {
+                            return (
+                                <div className={`${styles.badge} position-absolute badge bg-danger`}>
+                                    {status}
+                                </div>
+                            );
+                        } else if (status === "Alive") {
+                            return (
+                                <div className={`${styles.badge} position-absolute badge bg-success`}>
+                                    {status}
+                                </div>
+                            );
+                        } else {
+                            return (
+                                <div className={`${styles.badge} position-absolute badge bg-secondary`}>
+                                    {status}
+                                </div>
+                            );
+                        }
+                        })()}
+                    </div>
+                </Link>     
             );
         });
     }else {
